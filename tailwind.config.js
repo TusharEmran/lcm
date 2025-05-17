0a378c46d03e69814b23cca32a8c6e8629464ccb
TusharEmran <124170011+TusharEmran@users.noreply.github.com>
2025-05-17 10:50:52 +0600

    1st

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,61 @@
-@import "tailwindcss";
-
-/* Place any global CSS overrides or custom styles below */
-
+const config = {
+  darkMode: ["class"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          "2xl": "1400px",
+        },
+      },
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
+        },
         customgreys: {
-          primarybg: #1B1C22;
-          secondarybg: #25262F;
-          darkGrey: #17181D;
-          darkerGrey: #3d3d3d;
-          dirtyGrey: #6e6e6e;
+          primarybg: "#1B1C22",
+          secondarybg: "#25262F",
+          darkGrey: "#17181D",
+          darkerGrey: "#3d3d3d",
+          dirtyGrey: "#6e6e6e",
         },
         primary: {
           "50": "#fdfdff",
@@ -95,3 +143,7 @@
       },
     },
   },
+  plugins: [require("tailwindcss-animate"), "prettier-plugin-tailwindcss"],
+};
+
+module.exports = config;
